2d36885534c23f3368efa07cd563347017fd8981
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-26 17:15:28 -0700

    added grid prop values to the 2 different screen sizes

--- src/components/homePage/SearchGrid.js
+++ src/components/homePage/SearchGrid.js
@@ -1,17 +1,32 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
+import RegionOptions from "./RegionOptions";
+
 const SearchGrid = () => {
   return (
     <Grid
       container
-      sx={{ border: "yellow solid 3px", justifyContent: "space-between" }}
+      sx={{
+        //   border: "yellow solid 3px",
+        justifyContent: "space-between",
+      }}
     >
-      <Grid item xs={12} lg={4} sx={{ border: "red solid 2px" }}>
+      <Grid
+        item
+        xs={12}
+        lg={4}
+        //   sx={{ border: "red solid 2px" }}
+      >
         <p>Input 1</p>
       </Grid>
-      <Grid item xs={6} lg={2} sx={{ border: "green solid 2px" }}>
-        <p>Option Menu</p>
+      <Grid
+        item
+        xs={6}
+        lg={2}
+        //   sx={{ border: "green solid 2px" }}
+      >
+        <RegionOptions />
       </Grid>
     </Grid>
   );
