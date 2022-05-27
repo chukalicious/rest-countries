d10503f3aec94ed48bf95c498bfe0ff1e8162da7
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-26 22:55:57 -0700

    created menu to filter countries by region

--- src/components/homePage/RegionOptions.js
+++ src/components/homePage/RegionOptions.js
@@ -1,62 +1,33 @@
-import React from "react";
-
+import * as React from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-const currencies = [
-  {
-    value: "Europe",
-    label: "Europe",
-  },
-  {
-    value: "Africa",
-    label: "Africa",
-  },
-  {
-    value: "Asia",
-    label: "Asia",
-  },
-  {
-    value: "America",
-    label: "America",
-  },
-];
-
-const RegionOptions = () => {
-  const [currency, setCurrency] = React.useState("EUR");
+export default function SearchField() {
+  const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
-    setCurrency(event.target.value);
+    setAge(event.target.value);
   };
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          value={currency}
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Filter by Region</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="filter by region"
           onChange={handleChange}
-          helperText="Filter by Region"
         >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
+          <MenuItem value="Africa">Africa</MenuItem>
+          <MenuItem value="America">America</MenuItem>
+          <MenuItem value="Asia">Asia</MenuItem>
+        </Select>
+      </FormControl>
     </Box>
   );
-};
-
-export default RegionOptions;
+}
