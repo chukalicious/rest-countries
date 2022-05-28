9c0c34b857ac1363f225ef6e81f7c3597e12537f
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-28 16:35:18 -0700

    imported Page component to app.js. it will make the call to the api and display the country cards.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Box from "@mui/material/Box";
 import Navbar from "./components/common/Navbar";
 import SearchGrid from "./components/homePage/SearchGrid";
+import Page from "./components/homePage/Page";
 
 function App() {
   return (
     <Box>
       <Navbar />
       <SearchGrid />
+      <Page />
     </Box>
   );
 }
